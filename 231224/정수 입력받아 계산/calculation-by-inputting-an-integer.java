32c9d1c08eda33f90c4c442657c83de1f1d413a5
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-24 18:36:15 +0900

    Title: 정수 입력받아 계산, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231224/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        a *= 2;
+        a += 3;
+
+        System.out.println(a);
+    }
+}
