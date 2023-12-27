c00972b3f46225b6b4472e7e3222143bcd91fb5e
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-27 09:15:33 +0900

    Title: 삼항연산자 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231227/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a == 1) {
+            System.out.println("t");
+        } else {
+            System.out.println("f");
+        }
+
+        sc.close();
+    }
+}
