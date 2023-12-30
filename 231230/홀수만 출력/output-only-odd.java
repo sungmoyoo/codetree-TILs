29ea9fea3a061e894a3c677e0131da6a7856d9c7
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-30 19:10:49 +0900

    Title: 홀수만 출력, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231230/홀수만 출력/output-only-odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = a; i <= 9; i += 2) {
+            System.out.printf("%d ",i);
+        }
+
+    }
+}
