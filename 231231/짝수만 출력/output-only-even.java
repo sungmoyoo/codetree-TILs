e962f555a89cc8abbaa209bec3e65763c5fdef07
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-31 16:04:47 +0900

    Title: 짝수만 출력, Time: 184ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231231/짝수만 출력/output-only-even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while (a <= b) {
+            System.out.printf("%d ", a);
+            a += 2;
+        }
+        sc.close();
+    }
+}
