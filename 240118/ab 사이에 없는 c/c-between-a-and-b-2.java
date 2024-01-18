3fbc94969eccbc90da7a91f7777149b873a44a15
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-01-18 20:01:51 +0900

    Title: ab 사이에 없는 c, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        boolean ok = true;
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0) {
+                ok = false;
+            } 
+        }
+
+        if (ok == true) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
