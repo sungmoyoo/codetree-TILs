ac27ab0d1188376490c2cfbe5d6ba86fced3bd7d
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-02-07 09:14:12 +0900

    Title: 별표 출력하기 8, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240207/별표 출력하기 8/print-star-8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <=n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            } else {
+                System.out.println("*");
+            }
+        }
+        sc.close();
+    }
+}
