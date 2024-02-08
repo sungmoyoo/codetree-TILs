46d90abca2e0c231d2bc639f1ecad6f7b6e25d62
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-02-08 10:08:14 +0900

    Title: 별표 출력하기 11, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/별표 출력하기 11/print-star-11.java
+++ 240208/별표 출력하기 11/print-star-11.java
@@ -6,12 +6,17 @@ public class Main {
 
         int n = sc.nextInt();
 
-        for (int i = 1; i < 3*n-1; i++) {
-            for (int j = 1; j < 3*n-1; j++)
-            if (i % 2 == 0 && j % 2 ==0) {
-                System.out.print("  ");
-            } else {
-                System.out.print("* ");
+        for (int i = 1; i <= 2*n+1; i++) {
+            for (int j = 1; j <= 2*n+1; j++) {
+                if (i % 2 == 0 && j % 2 == 0) {
+                    if (i != 2*n+1 && j != 2*n+1) {
+                        System.out.print("  ");
+                    }
+                } else {
+                     System.out.print("* ");
+                }
+                
+                
             }
             System.out.println();
         }
