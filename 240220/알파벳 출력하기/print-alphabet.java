a819140dbdf24dc5b67851d1c29735e1b084390d
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-02-20 09:20:28 +0900

    Title: 알파벳 출력하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240220/알파벳 출력하기/print-alphabet.java
+++ 240220/알파벳 출력하기/print-alphabet.java
@@ -9,7 +9,11 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print((char) x++);
+                System.out.print((char) x);
+                ++x;
+                if (x == 91) {
+                    x = 'A';
+                }
             }
             System.out.println();
         }
