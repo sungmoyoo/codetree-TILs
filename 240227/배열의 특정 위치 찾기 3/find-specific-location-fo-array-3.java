af2c428904ea9585e931561547a297a40306aa17
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-02-27 10:19:01 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240227/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int sum = 0;
+        int cnt = 0;
+        
+        ArrayList<Integer> list = new ArrayList<>();
+    
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (true) {
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0) {
+                break;
+            }
+            list.add(n);        
+            cnt++;
+        }
+
+        for (int i = cnt-1; i >= 0; i--) {
+            sum += list.get(i);
+        }
+
+        bw.write(sum+"\n");
+
+        br.close();
+        bw.close();
+    }
+}
