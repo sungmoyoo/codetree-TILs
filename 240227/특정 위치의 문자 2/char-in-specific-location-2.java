57d20c0c03fcc1473469cbdd551e41b5f5607f9e
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-02-27 10:32:02 +0900

    Title: 특정 위치의 문자 2, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240227/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.next();
+        }
+        System.out.printf("%s %s %s", arr[1], arr[4], arr[7]);
+
+        sc.close();
+    }
+}
