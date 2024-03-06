87b37bb1713ca371ccb9dc222322c37fbaa2257b
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-03-06 10:01:39 +0900

    Title: 배열 놀이, Time: 175ms, Memory: 11MB, Status: Passed - CodeTree

--- 240306/배열 놀이/play-with-array.java
+++ 240306/배열 놀이/play-with-array.java
@@ -6,9 +6,9 @@ public class Main {
 
         int n = sc.nextInt();
         int q = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[n+1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = sc.nextInt();
         }
 
@@ -18,20 +18,29 @@ public class Main {
 
             if (no == 1) {
                 int a = sc.nextInt();
-                System.out.println(arr[a-1]);
+                System.out.println(arr[a]);
+
             } else if (no == 2) {
                 int a = sc.nextInt();
-                for (int j = 1; j <= arr.length; j++) {
-                    if (arr[j-1] == a) {
-                        System.out.println(j);
+                int e = -1;
+                for (int j = 1; j <= n; j++) {
+                    if (arr[j] == a) {
+                        e = j;
                         break;
                     }
                 }
+                if (e == -1) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(e);
+                }
+                
+
             } else if (no == 3) {
                 int a = sc.nextInt();
                 int b = sc.nextInt();
                 for (int j = a; j <= b; j++) {
-                    System.out.print(arr[j-1] + " ");
+                    System.out.print(arr[j] + " ");
                 }
                 System.out.println();
             }
