eaaf2b45387ebb84df2942a65dedb1380588d85f
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-03-09 23:35:32 +0900

    Title: n개의 숫자 중 최대 2개, Time: 181ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240309/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-1; j++) {
+                if (arr[j] < arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        
+        bw.write(arr[0] + " " + arr[1] + "\n");
+        bw.close();
+        br.close();
+    }
+}
