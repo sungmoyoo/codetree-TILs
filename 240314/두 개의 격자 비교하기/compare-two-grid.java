2ea28c7ae0643245bfa4a8eb7de798f0df7758d9
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-03-14 09:48:10 +0900

    Title: 두 개의 격자 비교하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+    
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == sc.nextInt()) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(1 + " ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
