0cca2ec1e92ef7a8a6391c94432a4547bb888b3e
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-03-22 09:39:33 +0900

    Title: 공백을 기준으로 출력, Time: 277ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240322/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        String str = str1.concat(str2);
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                System.out.print(str.charAt(i));
+            }
+        }
+
+        sc.close();
+    }
+}
