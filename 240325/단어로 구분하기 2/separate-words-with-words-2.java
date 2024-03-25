37d4b81842d269d757aaeebaa4bc3e246d69d3c0
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-03-25 09:41:19 +0900

    Title: 단어로 구분하기 2, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240325/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            String str = sc.next();
+            if (i % 2 == 0) {
+                System.out.println(str);
+            }
+        }
+        sc.close();
+    }
+}
