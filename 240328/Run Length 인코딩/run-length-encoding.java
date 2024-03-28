d39a07f1a509f9cc17b5ca6d2e8ad27faa105a1e
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-03-28 21:03:03 +0900

    Title: Run Length 인코딩, Time: 142ms, Memory: 11MB, Status: Passed - CodeTree

--- 240328/Run Length 인코딩/run-length-encoding.java
+++ 240328/Run Length 인코딩/run-length-encoding.java
@@ -4,25 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String A = sc.nextLine();
+        String A = sc.next();
         int count = 0;
         ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < A.length()-1; i++) {
-            if (i == 0) {
-                list.add(String.valueOf(A.charAt(i)));
-            }
 
-            if (A.charAt(i) == A.charAt(i+1)) {
-                count++;
-                if (i+1 == A.length()-1) {
+        list.add(String.valueOf(A.charAt(0)));
+        if (A.length() == 1) {
+            list.add(String.valueOf(++count));
+        } else {
+            for (int i = 0; i < A.length()-1; i++) {
+                if (A.charAt(i) == A.charAt(i+1)) {
+                    count++;
+                    if (i+1 == A.length()-1) {
+                        list.add(String.valueOf(++count));
+                    }
+                } else {
                     list.add(String.valueOf(++count));
-                }
-            } else {
-                list.add(String.valueOf(++count));
-                list.add(String.valueOf(A.charAt(i+1)));
-                count = 0;
-                if (i+1 == A.length()-1) {
-                    list.add(String.valueOf(++count));
+                    list.add(String.valueOf(A.charAt(i+1)));
+                    count = 0;
+                    if (i+1 == A.length()-1) {
+                        list.add(String.valueOf(++count));
+                    }
                 }
             }
         }
@@ -34,5 +36,6 @@ public class Main {
         System.out.println(result);
 
         sc.close();
+        
     }
 }
