33d3c23e9af32ac480c784b91098b2944921ebeb
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-03-29 09:27:06 +0900

    Title: 문자열 추가하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240329/문자열 추가하기/add-spring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        input += "Hello";
+
+        System.out.println(input);
+
+        sc.close();
+        
+    }
+}
