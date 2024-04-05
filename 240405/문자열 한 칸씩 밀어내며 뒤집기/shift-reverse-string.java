e35436ebc4e430f01a7dab327f70be45b5a93dad
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-04-05 14:20:27 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 178ms, Memory: 19MB, Status: Passed - CodeTree

--- 240405/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 240405/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -5,27 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String s = st.nextToken();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder s = new StringBuilder(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < q; i++) {
             int no = Integer.parseInt(br.readLine());
 
             if (no == 1) {
-                s = s.substring(1) + s.substring(0,1);
+                s.append(s.charAt(0)).deleteCharAt(0);
                 bw.write(s + "\n");
             } else if (no == 2) {
-                s = s.substring(s.length()-1) + s.substring(0, s.length()-1);
+                s.insert(0,s.charAt(s.length()-1)).deleteCharAt(s.length()-1);
                 bw.write(s + "\n");
             } else if (no == 3) {
-                char[] arr = s.toCharArray();
-                String result = "";
-                for (int j = arr.length-1; j >= 0; j--) {
-                    result += arr[j];
-                }
-                s = result;
+                s.reverse();
                 bw.write(s + "\n");
             }
         }
