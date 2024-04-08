ef19c86265aa98442aa6726d5debbc9515902851
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-04-08 09:39:29 +0900

    Title: 대문자로 출력하기, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240408/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) < 'Z' || s.charAt(i) > 'a' && s.charAt(i) < 'z') {
+                result += s.charAt(i);
+            } 
+        }
+
+        
+
+        System.out.println(result.toUpperCase());
+
+        sc.close();
+    }
+}
