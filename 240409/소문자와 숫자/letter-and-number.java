606ef80784b4849f56348e7a3cf901b0a6dc114f
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-04-09 09:22:52 +0900

    Title: 소문자와 숫자, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/소문자와 숫자/letter-and-number.java
+++ 240409/소문자와 숫자/letter-and-number.java
@@ -9,7 +9,7 @@ public class Main {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) >= 48 && s.charAt(i) <= 57 || 
                 s.charAt(i) >= 65 && s.charAt(i) <= 90 ||
-                s.charAt(i) >= 97 && s.charAt(i) <= 120) {
+                s.charAt(i) >= 97 && s.charAt(i) <= 122) {
                     result += s.charAt(i);
                 }
         }
