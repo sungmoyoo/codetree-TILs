639990509f77d34cb06ce05c65a3f2aee353f0f4
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-04-10 21:59:07 +0900

    Title: 정수만 추출하기, Time: 197ms, Memory: 10MB, Status: Passed - CodeTree

--- 240410/정수만 추출하기/extract-only-integers.java
+++ 240410/정수만 추출하기/extract-only-integers.java
@@ -8,17 +8,19 @@ public class Main {
         String s2 = sc.next();
 
         for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) <= '0' || s1.charAt(i) >= '9') {
-                s1 = s1.substring(0,i);
+            if (s1.charAt(i) >= '0' && s1.charAt(i) <= '9') {
+            } else {
+                s1 = s1.substring(0, i);
             }
             
         }
         for (int i = 0; i < s2.length(); i++) {
-            if (s2.charAt(i) <= '0' || s2.charAt(i) >= '9') {
-                s2 = s2.substring(0,i);
+            if (s2.charAt(i) >= '0' && s2.charAt(i) <= '9') {
+            } else {
+                s2 = s2.substring(0, i);
             }
         }
-        
+
         System.out.println(Integer.parseInt(s1) + Integer.parseInt(s2));
 
         sc.close();
