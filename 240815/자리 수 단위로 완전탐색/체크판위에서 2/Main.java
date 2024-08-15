25d30fa7d57ef9e01605a56cd8325f425dfaf03d
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-08-15 23:17:40 +0900

    완전탐색1: 자리 수 단위로 완전탐색

---
+++ 240815/자리 수 단위로 완전탐색/체크판위에서 2/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int R = sc.nextInt();
+    int C = sc.nextInt();
+    char[][] arr = new char[R][C];
+
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        arr[i][j] = sc.next().charAt(0);
+      }
+    }
+    int count = 0;
+
+
+
+    for (int i = 1; i < R; i++) {
+      for (int j = 1; j < C; j++) {
+        for (int k = i+1; k < R-1; k++) {
+          for (int l = j+1; l < C-1; l++) {
+            if (arr[0][0] != arr[i][j] && arr[i][j] != arr[k][l] && arr[k][l] != arr[R-1][C-1]) {
+              count++;
+            }
+          }
+        }
+      }
+    }
+    System.out.println(count);
+  }
+}
