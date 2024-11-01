f5ca81011398542ee59c54bc5aa08b6793790450
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-11-01 14:57:35 +0900

    Ad-Hoc: 고려해야 할 대상이 뚜렷이 정해지는 경우

---
+++ 241101/고려해야 할 대상이 뚜렷이 정해지는 경우/두 점으로 만드는 정사각형/Main.py
@@ -0,0 +1,11 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+min_x = min(x1, a1)
+min_y = min(y1, b1)
+max_x = max(x2, a2)
+max_y = max(y2, b2)
+
+length = max(max_x - min_x, max_y - min_y)
+
+print(length**2)
